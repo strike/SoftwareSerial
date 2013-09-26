ba15822634e7e69233c2d12dc28ce697039def40
Zubarev Alexander aka Strike <actionstrike@gmail.com>
2013-09-26 14:57:22 +0400

    dirty fix examples

--- examples/SoftwareSerialExample/SoftwareSerialExample.ino
+++ examples/SoftwareSerialExample/SoftwareSerialExample.ino
@@ -34,10 +34,6 @@ void setup()
 {
   // Open serial communications and wait for port to open:
   Serial.begin(57600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-
 
   Serial.println(F("Goodnight moon!"));
 
--- examples/TwoPortReceive/TwoPortReceive.ino
+++ examples/TwoPortReceive/TwoPortReceive.ino
@@ -47,10 +47,6 @@ void setup()
 {
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
-
 
   // Start each software serial port
   portOne.begin(9600);
